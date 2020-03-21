5aceee3ce5a88910aef1944eef8aa5dda3df38b2
anybody <dwiyual@gmail.com>
2020-03-20 20:05:37 -0500

    step 2 higher order functions done

--- challenges/functions.js
+++ challenges/functions.js
@@ -16,6 +16,7 @@ function consume(a, b, cb) {
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+let add = consume(num1, num2, (num1, num2) => {num1 * num2;});
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
